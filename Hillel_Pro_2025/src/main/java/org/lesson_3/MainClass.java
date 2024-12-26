58c1a954412696e12db0109d47296e1d4c6ecd5a
dmyt2024 <dmitrijnen@gmail.com>
2024-12-26 21:55:23 +0200

    Convertion celsius to farkengate

--- Hillel_Pro_2025/src/main/java/org/lesson_3/MainClass.java
+++ Hillel_Pro_2025/src/main/java/org/lesson_3/MainClass.java
@@ -1,18 +1,17 @@
 package org.lesson_3;
 
 public class MainClass {
-    public static final int FAR = 32;
 
     public static void main(String[] args) {
         System.out.println("Converter temperature.");
 
-        int cel = 50;
-        int far = convCelsToFarkengate(cel);
+        int celsius = 50;
+        int far = convCelsToFarkengate(celsius);
         System.out.println("Farkengate temperature is " + far + " degrees.");
 
     }
 
-    public static int convCelsToFarkengate(int cel) {
-        return (cel * 9 / 5) + 32;
+    public static int convCelsToFarkengate(int celsius) {
+        return (celsius * 9 / 5) + 32;
     }
 }
