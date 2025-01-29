f76cba57b8c39f6ec518229b306e3b4e5d7c3ba1
dmyt2024 <dmitrijnen@gmail.com>
2025-01-29 18:18:00 +0200

    Solution HW_10_IO

---
+++ Hillel_Pro_2025/src/main/java/org/lesson_10/FileHandler.java
@@ -0,0 +1,32 @@
+package org.lesson_10;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+    private final static String BASE_PATH = "src/main/java/org/lesson_10/";
+
+    public String writeFile(String fileName, String fileContent) {
+        try (FileWriter fw = new FileWriter(BASE_PATH + fileName + ".txt")) {
+            fw.write(fileContent);
+            return "Success.";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public String readFile(String path) {
+        try (FileReader reader = new FileReader(path)) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            int sym;
+            while ((sym = reader.read()) != -1) {
+                stringBuilder.append((char) sym);
+            }
+            return stringBuilder.toString();
+        } catch (IOException ex) {
+            return ex.getMessage();
+        }
+    }
+}
