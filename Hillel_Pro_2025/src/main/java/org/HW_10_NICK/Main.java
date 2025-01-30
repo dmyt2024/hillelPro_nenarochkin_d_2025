da59f09420f841b82ad6199ed4ea4a66501d9407
dmyt2024 <dmitrijnen@gmail.com>
2025-01-30 19:32:30 +0200

    Solution HW NICK

---
+++ Hillel_Pro_2025/src/main/java/org/HW_10_NICK/Main.java
@@ -0,0 +1,15 @@
+package org.HW_10_NICK;
+
+public class Main {
+    public static void main(String[] args) {
+        FileLoggerConfigurationLoader loader = new FileLoggerConfigurationLoader();
+        FileLoggerConfiguration config = loader.load("src/main/java/org/HW_10_NICK/config.txt");
+
+        FileLogger logger = new FileLogger(config);
+
+        logger.info("Programm beging");
+        logger.debug("Connection...");
+        logger.debug("Loading end.");
+        logger.info("Operation finished correctly.");
+    }
+}
