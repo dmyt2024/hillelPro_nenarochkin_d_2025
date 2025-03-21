0763d1b1ecca62b642a1549d61037e413dc8559c
dmyt2024 <dmitrijnen@gmail.com>
2025-03-21 12:49:37 +0200

    Remove .DS_Store and .idea from version control

--- Hillel_Pro_2025/src/main/java/org/lesson_19/ArrayUtils.java
+++ Hillel_Pro_2025/src/main/java/org/lesson_19/ArrayUtils.java
@@ -65,6 +65,7 @@ public class ArrayUtils {
     public int binarySearch(int[] array, int target) {
         int left = 0;
         int right = array.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
             for (int i = 0; i < array.length; i++) {
