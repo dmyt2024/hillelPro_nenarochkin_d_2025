a3a62e92467672cbb08e21e1ced2b3b126f50bd8
dmyt2024 <dmitrijnen@gmail.com>
2025-04-05 17:24:31 +0300

    Less 26 Expert

---
+++ Hillel_Pro_2025/src/main/java/org/lesson_26_Expert/Address.java
@@ -0,0 +1,14 @@
+package org.lesson_26_Expert;
+
+public class Address {
+
+    private String address;
+
+    public Address(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
