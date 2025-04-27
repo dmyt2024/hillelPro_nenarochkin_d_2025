e4cc7b44cfb2a21b75baaf1929840c673c3834d0
dmyt2024 <dmitrijnen@gmail.com>
2025-04-27 23:26:10 +0300

    Lesson 31 JUnitTest

---
+++ Hillel_Pro_2025/src/main/java/org/lesson_31_JUnitTest/Library.java
@@ -0,0 +1,36 @@
+package org.lesson_31_JUnitTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Library {
+
+    List<Book> books = new ArrayList<Book>();
+
+
+    public void addBook(Book book) {
+        if (book == null) {
+            throw new IllegalArgumentException("Book cannot be null");
+        }
+        books.add(book);
+
+    }
+
+    public boolean removeBook(Book book) {
+        if (book == null) {
+            throw new IllegalArgumentException("Book cannot be null");
+        }
+        return books.remove(book);
+    }
+
+    public List<Book> getBooks() {
+
+        return Collections.unmodifiableList(books);
+    }
+
+    public int getBookCount() {
+
+        return books.size();
+    }
+}
