86c3a8e45d556f5e286fc9ef35ed4dee9154545e
dmyt2024 <dmitrijnen@gmail.com>
2025-06-01 17:33:37 +0300

    Unversioned Files

---
+++ Hillel_Pro_2025/orderMVC/src/test/java/org/dmytr/ordermvc/OrderMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dmytr.ordermvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
