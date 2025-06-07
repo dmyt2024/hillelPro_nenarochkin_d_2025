590d4235659bf3efcc2c57acdf671e67e6f48048
dmyt2024 <dmitrijnen@gmail.com>
2025-06-07 08:11:13 +0100

    commit Unversioned files

--- Hillel_Pro_2025/less_42_Spring_data_JPA/src/main/java/org/dmytr/less_42_spring_data_jpa/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package org.dmytr.less_42_spring_data_jpa.controller;
-
-public class UserController {
-}
--- Hillel_Pro_2025/less_42_Spring_data_JPA/src/main/java/org/dmytr/less_42_spring_data_jpa/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.dmytr.less_42_spring_data_jpa.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-}
