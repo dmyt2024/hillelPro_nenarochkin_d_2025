64ad4bf43cc78bcc42177eca8376ff3c72b19f25
dmyt2024 <dmitrijnen@gmail.com>
2025-06-10 16:33:02 +0300

    add commit files lesson 43 Spring Security

---
+++ Hillel_Pro_2025/less_43_Spring_security/src/test/java/org/dmytr/less_43_spring_security/Less43SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dmytr.less_43_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Less43SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
